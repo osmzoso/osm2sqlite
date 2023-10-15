00d376a9369c68273fcada2b1cbab57c19f0dea3
osmzoso <62518847+osmzoso@users.noreply.github.com>
2023-10-15 11:24:24 +0200

    Add files via upload

--- test/test1_read_data.sh
+++ test/test1_read_data.sh
@@ -1,11 +1,9 @@
 #
-# Testcase1
-#
-# Read compressed date with both versions
+# Test1: Read OSM XML data
 #
 if [ $# = 0 ]
 then
-    echo "error: no filename specified"
+    echo "error: no .osm.bz2 file specified"
     echo "Usage:"
     echo "$0 FILE_OSM_XML_BZ2"
     exit 1
@@ -13,23 +11,16 @@ fi
 file_osm_xml_bz2=$1
 
 echo "----------------------------------------------"
-echo "Help Output C-Version"
+echo "Test1: Read OSM XML data"
 echo "----------------------------------------------"
-../osm2sqlite
-echo "----------------------------------------------"
-echo "Help Output Python-Version"
-echo "----------------------------------------------"
-../osm2sqlite.py
-
-echo "----------------------------------------------"
-echo "Test1: Read OSM XML $file_osm_xml_bz2"
-echo "----------------------------------------------"
+echo ".osm.bz2 file : " $file_osm_xml_bz2
 rm -f osm_c.db osm_py.db
 set -x              # activate bash debugging mode
 time bzip2 -c -d $file_osm_xml_bz2 | ../osm2sqlite - osm_c.db addr graph
 time bzip2 -c -d $file_osm_xml_bz2 | ../osm2sqlite.py - osm_py.db addr graph
 set +x              # stop bash debugging mode
-echo "Size and MD5-Hash of the databases:"
+echo "compare size of databases:"
 ls -l *.db
+echo "compare MD5 hash values of the databases:"
 md5sum *.db
 
