4dc358554e91fd4fb14b81c8bd8ca589940373bd
osmzoso <62518847+osmzoso@users.noreply.github.com>
2023-12-12 13:55:39 +0100

    Add files via upload

--- test/test1_read_data.sh
+++ test/test1_read_data.sh
@@ -16,8 +16,8 @@ echo "----------------------------------------------"
 echo ".osm.bz2 file : " $file_osm_xml_bz2
 rm -f osm_c.db osm_py.db
 set -x              # activate bash debugging mode
-time bzip2 -c -d $file_osm_xml_bz2 | ../osm2sqlite - osm_c.db addr graph
-time bzip2 -c -d $file_osm_xml_bz2 | ../osm2sqlite.py - osm_py.db addr graph
+time bzip2 -c -d $file_osm_xml_bz2 | ../src/osm2sqlite - osm_c.db addr graph
+time bzip2 -c -d $file_osm_xml_bz2 | ../src_py/osm2sqlite.py - osm_py.db addr graph
 set +x              # stop bash debugging mode
 echo "compare size of databases:"
 ls -l *.db
