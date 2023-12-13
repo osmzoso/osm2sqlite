3210bb5f741b3363c718220f3ccfdd72e4751d27
osmzoso <62518847+osmzoso@users.noreply.github.com>
2023-12-13 14:30:52 +0100

    Add files via upload

--- example_db.sh
+++ example_db.sh
@@ -2,40 +2,36 @@
 #
 # This file shows an example workflow
 #
-# Directories for the XML files, databases and evaluation results
-dir_xml_bz2="$HOME/osm/xml"
-dir_database="$HOME/osm/database"
-dir_results="$HOME/osm/results"
+db="$HOME/osm/database/freiburg-regbez-latest.db"
+osm="$HOME/osm/xml/freiburg-regbez-latest.osm.bz2"
 # String for file name Date
 cdate=$(date '+%Y%m%d')
 #
-# name of the bz2 compressed xml file
-name="freiburg-regbez-latest"
-db="$dir_database/$name.db"
-#
 # Read data and create a new database
+#  (table 'graph' is required)
 #
 rm $db
-time bzip2 -c -d "$dir_xml_bz2/$name.osm.bz2" | ./src/osm2sqlite - $db addr rtree-ways graph
+time bzip2 -c -d $osm | ./src/osm2sqlite - $db addr rtree-ways graph
+sqlite3 $db < ./src_py/map_def.sql
 #
-# Routing (table 'graph' is required)
+# Routing
 #
-./src_py/route.py $db 7.808 47.983 7.889 47.897 > "$dir_results/freiburg_schauinsland.csv"
-./tools/html_map_csv.py "$dir_results/freiburg_schauinsland.csv" > "$dir_results/$cdate-map_route_freiburg_schauinsland.html"
-./tools/convert_csv2gpx.py "$dir_results/freiburg_schauinsland.csv" > "$dir_results/freiburg_schauinsland.gpx"
+./src_py/route.py $db 7.808 47.983 7.889 47.897 > "$HOME/osm/results/freiburg_schauinsland.csv"
+./tools/html_map_csv.py "$HOME/osm/results/freiburg_schauinsland.csv" > "$HOME/osm/results/$cdate-map_route_freiburg_schauinsland.html"
+./tools/convert_csv2gpx.py "$HOME/osm/results/freiburg_schauinsland.csv" > "$HOME/osm/results/freiburg_schauinsland.gpx"
 #
 # Tilemaps with Leaflet.js
 #
 # create HTML file with a map of the addresses
-./tools/html_map_addr.py $db 7.791 47.975 7.809 47.983 > "$dir_results/$cdate-map_adressen_freiburg_st_georgen.html"
+./tools/html_map_addr.py $db 7.791 47.975 7.809 47.983 > "$HOME/osm/results/$cdate-map_adressen_freiburg_st_georgen.html"
 # create HTML file with a map of the routing graph
-./tools/html_map_table_graph.py $db 7.81 47.97 7.83 47.98 > "$dir_results/$cdate-map_table_graph_freiburg.html"
+./tools/html_map_table_graph.py $db 7.81 47.97 7.83 47.98 > "$HOME/osm/results/$cdate-map_table_graph_freiburg.html"
 #
 # Check addr:street name
 #
-./src_py/check_addr_street_name.py $db 791% > "$dir_results/$cdate-error_addr_street_name.html"
+./src_py/check_addr_street_name.py $db 791% > "$HOME/osm/results/$cdate-error_addr_street_name.html"
 #
 # Draw map
 #
-sqlite3 $db < ./src_py/map_def.sql 
-./src_py/map.py $db 7.807 47.982 16 1300 900 > "$dir_results/$cdate-map_zoom16.svg"
+./src_py/map.py $db 7.807 47.982 16 1300 900 > "$HOME/osm/results/$cdate-map_zoom16.svg"
+./src_py/map.py $db 7.572 48.033 16 1300 900 > "$HOME/osm/results/$cdate-map_zoom16_breisach.svg"
